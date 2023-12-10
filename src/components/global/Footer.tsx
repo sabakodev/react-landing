d292634db93fce070b21562fa51cf02d9740cd8f
Fray Dalton <fray117@exploiter.id>
2023-12-10 23:08:43 +0700

    remove dark selector completely

--- src/components/global/Footer.tsx
+++ src/components/global/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function Footer() {
 	return (
 		<div className="z-10 max-w-5xl w-full font-mono text-sm">
-			<div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black">
+			<div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-black via-black">
 				<Link
 					className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto"
 					href="/"
--- src/components/landing/Card.tsx
+++ src/components/landing/Card.tsx
@@ -13,7 +13,7 @@ export default function Card({ cards }: { cards: Card[] }) {
 				cards.map((card, index) => (
 					<Link key={index}
 						href={card.href}
-						className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+						className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-neutral-700 hover:bg-neutral-800/30"
 					>
 						<h2 className={`mb-3 text-2xl font-semibold`}>
 							{card.title + ' '}
