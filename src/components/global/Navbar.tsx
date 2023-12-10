c0eb2f93253f3af49e5d80184362e571d574a265
Fray Dalton <fray117@exploiter.id>
2023-12-10 22:59:33 +0700

    update navbar

--- src/components/global/Navbar.tsx
+++ src/components/global/Navbar.tsx
@@ -4,6 +4,7 @@ import Link from "next/link"
 import Image from "next/image"
 import { Disclosure } from "@headlessui/react"
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline"
+import useScroll from "@/lib/hooks/useScroll"
 
 export type Link = {
 	href: string
@@ -11,6 +12,62 @@ export type Link = {
 }
 
 export default function Navbar({ links }: { links: Link[] }) {
+	const scrolled = useScroll(50)
+
+	return (
+		<div className={`fixed top-0 w-full pt-6 pb-4 z-50 ${scrolled ? 'backdrop-blur-xl bg-neutral-800/50' : 'bg-neutral-800/0'} transition-all`}>
+			<Disclosure as="nav" className="relative grid grid-cols-7 lg:max-w-7xl mx-auto">
+				{({ open }) => (
+					<>
+						<div className="flex items-center justify-between sm:hidden col-span-7 px-4">
+							<Link href="/" className="flex justify-center items-center space-x-2">
+								<Image src="/sabako-light.svg" alt="" height={256} width={256} className="h-8 w-8" />
+								<h1 className="text-2xl font-bold font-mono">SABAKO</h1>
+							</Link>
+							<Disclosure.Button className="relative border border-neutral-800 rounded-md bg-zinc-800/30 hover:bg-zinc-800 p-2 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+								<span className="absolute -inset-0.5" />
+								<span className="sr-only">Open main menu</span>
+								{open ? (
+									<XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+								) : (
+									<Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+								)}
+							</Disclosure.Button>
+						</div>
+						<Link href="/" className="hidden sm:flex justify-start items-center space-x-2 mb-4 col-span-2">
+							<Image src="/sabako-light.svg" alt="" height={128} width={128} className="h-10 w-10" />
+							<h1 className="text-2xl font-bold font-mono">SABAKO</h1>
+						</Link>
+						<div className="hidden sm:flex w-full items-center justify-end space-x-8 lg:static lg:w-auto font-mono col-span-5">
+							{
+								links.map((link, index) => (
+									<Link key={index} href={link.href} className="hover:text-neutral-50 hover:underline underline-offset-8">{link.label}</Link>
+								))
+							}
+						</div>
+
+						<Disclosure.Panel className="sm:hidden col-span-7 transition">
+							<div className={`space-y-1 px-2 pb-3 pt-2 mt-4 h-full w-full ${(scrolled || open) ? 'backdrop-blur-xl bg-neutral-800/50' : 'bg-neutral-800/0'} font-mono`}>
+								{links.map((link) => (
+									<Disclosure.Button
+										key={link.label}
+										as="a"
+										href={link.href}
+										className="text-neutral-300 hover:bg-neutral-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+									>
+										{link.label}
+									</Disclosure.Button>
+								))}
+							</div>
+						</Disclosure.Panel>
+					</>
+				)}
+			</Disclosure>
+		</div>
+	)
+}
+
+export function OldNavbar({ links }: { links: Link[] }) {
 	return (
 		<Disclosure as="nav" className="relative lg:max-w-5xl w-full mb-8">
 			{({ open }) => (
@@ -37,7 +94,7 @@ export default function Navbar({ links }: { links: Link[] }) {
 					<div className="hidden sm:flex w-full justify-around border-b pb-6 pt-8 backdrop-blur-2xl border-neutral-800 bg-zinc-800/30 from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:p-4 font-mono">
 						{
 							links.map((link, index) => (
-								<Link key={index} href={link.href} className="hover:text-gray-300 hover:underline underline-offset-8">{link.label}</Link>
+								<Link key={index} href={link.href} className="hover:text-neutral-300 hover:underline underline-offset-8">{link.label}</Link>
 							))
 						}
 					</div>
