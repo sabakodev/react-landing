fb9c46bf43b263eb80f3e5fbb838aaaff917ba61
Fray Dalton <fray117@exploiter.id>
2023-12-10 22:59:48 +0700

    add hero section

---
+++ src/components/landing/Hero.tsx
@@ -0,0 +1,62 @@
+import Link from "next/link"
+
+export default function Hero() {
+	return (
+		<div className="relative isolate px-6 lg:px-8">
+			<div
+				className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+				aria-hidden="true"
+			>
+				<div
+					className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+					style={{
+						clipPath:
+							'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+					}}
+				/>
+			</div>
+			<div className="mx-auto max-w-2xl pb-32 sm:pb-48 lg:pb-56 pt-16 sm:pt-20">
+				<div className="hidden sm:mb-8 sm:flex sm:justify-center">
+					<div className="relative rounded-full px-3 py-1 text-sm leading-6 text-neutral-300 ring-1 ring-white/10 hover:ring-white/20">
+						Announcing our partner project.{' '}
+						<a href="https://project.otabyte.net/" className="font-semibold text-fuchsia-600">
+							<span className="absolute inset-0" aria-hidden="true" />
+							Read more <span aria-hidden="true">&rarr;</span>
+						</a>
+					</div>
+				</div>
+				<div className="text-center">
+					<h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
+						Make your business integrated
+					</h1>
+					<p className="mt-6 text-lg leading-8 text-neutral-300">
+						Our team&#39;s vision prioritizes user experience, enabling limitless creativity to come to fruition and expand horizons, unlock the business to limitless.
+					</p>
+					<div className="mt-10 flex items-center justify-center gap-x-6">
+						<Link
+							href="/contact"
+							className="rounded-md bg-fuchsia-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-fuchsia-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-fuchsia-600"
+						>
+							Get started
+						</Link>
+						<Link href="/about" className="text-sm font-semibold leading-6 text-white">
+							Learn more <span aria-hidden="true">→</span>
+						</Link>
+					</div>
+				</div>
+			</div>
+			<div
+				className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+				aria-hidden="true"
+			>
+				<div
+					className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+					style={{
+						clipPath:
+							'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+					}}
+				/>
+			</div>
+		</div>
+	)
+}
