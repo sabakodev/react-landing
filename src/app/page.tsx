3156b9d1622cb3e35b4acaa56d639a2f047ad250
Fray Dalton <fray117@exploiter.id>
2023-12-10 23:00:29 +0700

    add hero component to landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,10 @@
+import { HeroComponent } from '@/components/landing'
 import Image from 'next/image'
 
 export default function Home() {
 	return (
-		<main className="relative place-items-center lg:max-w-5xl w-full mb-16">
-			<div className="relative place-items-center lg:max-w-5xl w-full mb-16">
-				<Image
-					src="/sky.jpg"
-					alt="SABAKO"
-					className="w-full h-72 object-cover object-bottom"
-					width={1000}
-					height={1000}
-					priority
-				/>
-				<div className="mt-2 text-sm text-gray-500">
-					Photos by&nbsp;
-					<a className="text-gray-300 hover:text-gray-400" href="https://unsplash.com/photos/3YrppYQPoCI">Guillaume Galtier via Unsplash</a>
-				</div>
-				<div className="mt-16 text-gray-300 leading-6">
-					Our team&#39;s vision prioritizes user experience, enabling limitless creativity to come to fruition and expand horizons, opening the business limitless.
-				</div>
-			</div>
+		<main className="w-full mb-16">
+			<HeroComponent />
 		</main>
 	)
 }
